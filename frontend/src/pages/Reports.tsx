67b59cda06c0f7ac78e8ae4969cc84bf47d52345
Vicente Bercasio <vicentetobio00@gmail.com>
2025-09-06 21:42:58 +0800

    Fix reports

--- frontend/src/pages/Reports.tsx
+++ frontend/src/pages/Reports.tsx
@@ -291,7 +291,6 @@ const fetchReports = async (force = false) => {
 
 
 useEffect(() => { fetchReports(); }, []);
-useEffect(() => { loadOptions('', ''); }, []);
 
   const loadOptions = async (barangaySearch = '', disasterSearch = '') => {
     try {
